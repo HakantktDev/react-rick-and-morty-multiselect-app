9869ca84ef19f9ef321f03d405a5e4d421ba07eb
Hakan <100341321+HakantktDev@users.noreply.github.com>
2024-05-20 15:18:27 +0300

    initial-commit

--- src/components/InstructionsList.tsx
+++ src/components/InstructionsList.tsx
@@ -1,4 +1,4 @@
-export const InstructionsList = () => {
+const InstructionsList = () => {
   return (
     <div className="d-flex-center">
       <ul className="instructions-list">
@@ -20,3 +20,4 @@ export const InstructionsList = () => {
     </div>
   );
 };
+export default InstructionsList
